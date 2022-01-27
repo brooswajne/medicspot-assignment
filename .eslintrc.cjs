0bcd5467fc76c496a3f3aa5e7e798593228a210a
Willem Garnier <willem.garnier@gmail.com>
2022-01-27 17:20:05 +0000

    add linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+	root: true,
+	parserOptions: { ecmaVersion: 2022 },
+	extends: [ "@brooswajne" ],
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import { PORT } from './server/config.js';
-import { createApplication } from './server/application.js';
+import { PORT } from "./server/config.js";
+import { createApplication } from "./server/application.js";
 
 const app = createApplication({ port: PORT });
 await app.start( );
