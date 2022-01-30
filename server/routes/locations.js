0fe684398798d43117a685cff2981d1bb9854686
Willem Garnier <willem.garnier@gmail.com>
2022-01-30 18:08:31 +0000

    server: fix parsing of locations search endpoint limit/offset params

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -10,11 +10,9 @@ export async function GET(request, { logger }, {
 	// for dependency injection
 	searchLocationsImpl = searchLocations,
 } = { }) {
-	const {
-		q: search,
-		limit,
-		offset,
-	} = request.query;
+	const { q: search } = request.query;
+	const limit = Number(request.query.limit);
+	const offset = Number(request.query.offset);
 
 	// TODO: just support getting _all_ locations probably
 	if (typeof search !== "string") throw new BadRequestError("Invalid search query");
@@ -22,8 +20,8 @@ export async function GET(request, { logger }, {
 	logger.debug(`Searching for locations matching query "${search}"`);
 	const locations = await searchLocationsImpl(search, {
 		fields: [ "name" ],
-		limit: limit && Number.isInteger(limit) ? Number(limit) : undefined,
-		offset: offset && Number.isInteger(offset) ? Number(offset) : undefined,
+		limit: Number.isInteger(limit) ? limit : undefined,
+		offset: Number.isInteger(offset) ? offset : undefined,
 	});
 	return locations.map((l) => l.name);
 }
